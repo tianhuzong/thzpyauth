60ea976d1aeb3ff1202ac0b6b6a21f84eba932a7
tianhuzong <tianhuzong@qq.com>
2024-03-10 09:35:21 +0000

    编写测试代码

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
-
+from django.test import TestCase,Client
+from django.urls import reverse
 # Create your tests here.
+
+class PublicTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+    def test_Service_state(self):
+        response = self.client.get(reverse("api:service_state"))
+        self.assertEqual(response.status_code,200)
--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import view,views
+
+app_name = "api"
+
 urlpatterns = [
     path('adduser/',view.adduser),
     path('service_state',views.service_state,name="service_state"),
