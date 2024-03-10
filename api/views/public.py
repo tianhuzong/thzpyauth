652a7a37782b22c603bdd3920cb5f76fb3ff3e87
tianhuzong <tianhuzong@qq.com>
2024-03-10 09:42:03 +0000

    编译扩展,将公有接口转向扩展

--- api/views/public.py
+++ api/views/public.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from globals_var import logger
-import utilpy.web
+import utils.web
 @api_view(["GET"])
 def service_state(request):
     logger.info((request.META))
-    return Response({"code":200,"msg":"succeed","data":{"ip":utilpy.web.get_real_ip(request),"UA":utilpy.web.get_ua(request)}})
+    return Response({"code":200,"msg":"succeed","data":{"ip":utils.web.get_real_ip(request),"UA":utils.web.get_ua(request)}})
